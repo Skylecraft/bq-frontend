2a105f593523ae3374a4bf65f819665ad71d7ebe
Vaesim <vaesimgames@gmail.com>
2022-02-03 13:10:27 -0300

    Update i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -1,15 +1,17 @@
 import { createI18n } from "vue3-i18n";
 import yaml from 'js-yaml'
 import enLang from './assets/translations/en.yml?raw'
+import esLang from './assets/translations/es.yml?raw'
 import ruLang from './assets/translations/ru.yml?raw'
 
 const messages = {
+  es: yaml.load(esLang),
   en: yaml.load(enLang),
   ru: yaml.load(ruLang),
 }
 
 const i18n = createI18n({
-  locale: "ru",
+  locale: "es",
   messages: messages,
 })
 
